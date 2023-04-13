ed68d770dee94af2bc7a879748a19d7a42fdab37
Jimmy <k1821317@kingston.ac.uk>
2023-04-13 13:47:42 +0100

    variable x and y size. Multithreading added

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -3,13 +3,24 @@
 
 #include <iostream>
 #include <windows.h>  
+#include <time.h>
+#include <omp.h>
 
-const int gridSize = 30 ;
+clock_t startTime, endTime;
+float processTime;
 
-int steps = 1000;
+HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
-bool presentCells[gridSize][gridSize];
-bool futureCells[gridSize][gridSize];
+const int gridSizeX = 20;
+const int gridSizeY = 20;
+
+int steps = 100000;
+
+int livingCellCount;
+int deadCellCount;
+
+bool presentCells[gridSizeX][gridSizeY];
+bool futureCells[gridSizeX][gridSizeY];
 
 bool randomBool() {
     return rand() > (RAND_MAX / 2);
@@ -18,20 +29,39 @@ bool randomBool() {
 void initialiseGrid()
 {
     srand(time(NULL));
-    for (int x = 0; x < gridSize; x++)
+//#pragma omp parallel for //breaks randomisation
+    for (int x = 0; x < gridSizeX; x++)
     {
-        for (int y = 0; y < gridSize; y++)
+        for (int y = 0; y < gridSizeY; y++)
         {
             presentCells[x][y] = randomBool();
         }
     }
 }
 
+void cellInfo()
+{
+    livingCellCount = 0;
+    deadCellCount = 0;
+    for (int x = 0; x < gridSizeX; x++)
+    {
+        for (int y = 0; y < gridSizeY; y++)
+        {
+            livingCellCount += presentCells[x][y];
+        }
+    }
+    deadCellCount = (gridSizeX * gridSizeY) - livingCellCount;
+
+    std::cout << "Living Cells: " << livingCellCount << " Dead Cells: " << deadCellCount << " Total Cells: " << livingCellCount + deadCellCount << "\n";
+    
+}
+
 void overwritePresentCells()
 {
-    for (int x = 0; x < gridSize; x++)
+#pragma omp parallel for //schedule(dynamic,10)//num_threads(20) // //schedule(guided,10) //num_threads(4)
+    for (int x = 0; x < gridSizeX; x++)
     {
-        for (int y = 0; y < gridSize; y++)
+        for (int y = 0; y < gridSizeY; y++)
         {
             presentCells[x][y] = futureCells[x][y];
         }
@@ -40,40 +70,53 @@ void overwritePresentCells()
 
 void displayCells()
 {
-    for (int y = 0; y < gridSize; y++)
+    for (int y = 0; y < gridSizeY; y++)
     {
-        for (int x = 0; x < gridSize; x++)
+        for (int x = 0; x < gridSizeX; x++)
         {
-            if (presentCells[x][y] == true) std::cout << "X ";
-            else std::cout << "O ";
+            if (presentCells[x][y] == true)
+            {
+                //system("Color 0A");
+                SetConsoleTextAttribute(h, 10);
+                std::cout << "X ";
+            }
+            else
+            {
+                //system("Color 0C");
+                SetConsoleTextAttribute(h, 12);
+                std::cout << ". ";
+            }
         }
         std::cout << "\n";
     }
 
-              std::cout << "\n";
     std::cout << "\n";
-    std::cout << "\n";
+    SetConsoleTextAttribute(h, 15);
     
 }
 
-void update()
+void processCells()
 {
-    for (int x = 0; x < gridSize; x++)
+#pragma omp parallel for //schedule(dynamic,10)//num_threads(20) //collapse(2) //schedule(guided,10) //num_threads(4)
+    for (int x = 0; x < gridSizeX; x++)
     {
-        for (int y = 0; y < gridSize; y++)
+        for (int y = 0; y < gridSizeY; y++)
         {
             bool alive;
             int neighbourCount = 0;
+
+            neighbourCount = presentCells[x - 1][y - 1] + presentCells[x][y - 1] + presentCells[x + 1][y - 1] + presentCells[x - 1][y] + presentCells[x + 1][y] + presentCells[x - 1][y + 1] + presentCells[x][y + 1] + presentCells[x + 1][y + 1];
+
             if (presentCells[x][y] == true)
             {
-                if (presentCells[x-1][y-1] == true) neighbourCount++;
-                if (presentCells[x][y-1] == true) neighbourCount++;
-                if (presentCells[x+1][y-1] == true) neighbourCount++;
-                if (presentCells[x-1][y] == true) neighbourCount++;
-                if (presentCells[x+1][y] == true) neighbourCount++;
-                if (presentCells[x-1][y+1] == true) neighbourCount++;
-                if (presentCells[x][y+1] == true) neighbourCount++;
-                if (presentCells[x+1][y+1] == true) neighbourCount++;
+                /*if (presentCells[x - 1][y - 1] == true) neighbourCount++;
+                if (presentCells[x][y - 1] == true) neighbourCount++;
+                if (presentCells[x + 1][y - 1] == true) neighbourCount++;
+                if (presentCells[x - 1][y] == true) neighbourCount++;
+                if (presentCells[x + 1][y] == true) neighbourCount++;
+                if (presentCells[x - 1][y + 1] == true) neighbourCount++;
+                if (presentCells[x][y + 1] == true) neighbourCount++;
+                if (presentCells[x + 1][y + 1] == true) neighbourCount++;*/
 
 
                 if (neighbourCount <= 1 || neighbourCount >= 4) alive = false;
@@ -83,38 +126,65 @@ void update()
             }
             else
             {
-                if (presentCells[x - 1][y - 1] == true) neighbourCount++;
+                /*if (presentCells[x - 1][y - 1] == true) neighbourCount++;
                 if (presentCells[x][y - 1] == true) neighbourCount++;
                 if (presentCells[x + 1][y - 1] == true) neighbourCount++;
                 if (presentCells[x - 1][y] == true) neighbourCount++;
                 if (presentCells[x + 1][y] == true) neighbourCount++;
                 if (presentCells[x - 1][y + 1] == true) neighbourCount++;
                 if (presentCells[x][y + 1] == true) neighbourCount++;
-                if (presentCells[x + 1][y + 1] == true) neighbourCount++;
+                if (presentCells[x + 1][y + 1] == true) neighbourCount++;*/
 
                 if (neighbourCount == 3) futureCells[x][y] = true;
             }
 
         }
     }
+}
+
+void update()
+{
+    processCells();
     overwritePresentCells();
+}
+
+void updateInfo()
+{
+    system("cls"); //Significantly hurt performance accidently leaving it on when not displaying cells
     displayCells();
-    //system("pause");
-    Sleep(100);
-    system("cls");
+    cellInfo();
+    system("pause");
+
+
+    //Sleep(200);
 }
 
 
 
 int main()
 {
+    //Initialisation
+    startTime = clock();
     initialiseGrid();
+    endTime = clock();
+    processTime = (((float)endTime - (float)startTime) / 1000);
+    std::cout << "Grid Initialisation process time: " << processTime << " (in s)" << std::endl;
     //generateghostbordercells
-    displayCells();
+    //displayCells();
+    cellInfo();
+    system("pause");
+    
+    
+    //Processing
+    startTime = clock();
     for (int i = 0; i < steps; i++) {
         update();
+        updateInfo();
     }
-    
+    endTime = clock();
+    processTime = (((float)endTime - (float)startTime) / 1000);
+    std::cout << "Simulation process time: " << processTime << " (in s)" << std::endl;
+    cellInfo();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
